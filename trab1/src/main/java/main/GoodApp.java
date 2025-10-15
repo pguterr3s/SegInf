6602e381835007ff23e90ef82668891a0ba8b893
P <pedrogu.terres27@gmail.com>
2025-10-15 11:14:01 +0100

    4.1 ALMOST DONE

--- trab1/src/main/java/main/GoodApp.java
+++ trab1/src/main/java/main/GoodApp.java
@@ -2,7 +2,6 @@ package main;
 
 public class GoodApp {
     private static final String msg = "Hello, I'm the good app";
-
     public static void main(String[] args){
         System.out.println(msg);
     }
